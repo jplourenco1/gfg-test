b62e1a44be8f24e7a19605c768389d37ed107a71
jplourenco1 <jplourenco@gmail.com>
2019-05-16 20:21:25 -0300

    Update gfg-cc.py

--- gfg-cc.py
+++ gfg-cc.py
@@ -1,4 +1,4 @@
-
+#GFG Code Challenge 
 file = open("bigfile",'r')
 data = file.read()
 lines = data.splitlines()
@@ -27,19 +27,17 @@ outfile.close()
 
 #4 Create a new tab-separated file holding fields 3, 6 and 13 from all lines in the original
 result1=[]
+outfile3 = open("question4.txt", "w")
 for line in lines:
     field3  = line.split('\t')[2]
     field6  = line.split('\t')[5]
     field13 = line.split('\t')[12]
-    result1.append('\t'.join([field3, field6, field13]))
-to_file2 = '\n'.join(result1)
-outfile3 = open("question4.txt", "a")
-outfile3.write(to_file2)
+    outfile3.write('\t'.join([field3, field6, field13]) + '\n')
 outfile3.close()
 
 #5 Create a new file which holds only even lines (2, 4, 6, etc.) from the original
 i = 1
-outfile2 = open("question5.txt", "a")
+outfile2 = open("question5.txt", "w")
 for line in lines:
     if i % 2 == 0 :
         outfile2.write(line + '\n')
@@ -50,7 +48,7 @@ outfile2.close()
 count=0
 for line in lines:
     field4  = line.split('\t')[3]
-    if field4 > 100 and field4 < 400:
+    if field4 > '100' and field4 < '400':
         count+=1
 print("Number of lines where field 4 is between 100 and 400", count)
 file.close()
